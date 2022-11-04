3497d4acf60755e6e199365581b916251770a542
rvaldez171 <rvaldez@solutionsda.com>
2022-11-04 09:59:19 -0600

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import express from "express";
 
 const app = express();
 
-app.listen(300);
-console.log("Corriendo en el puerto 3000");
-
 app.get('/', (req,res)=> {
     res.send('Hola mundo!')
+    
 })
+
+app.listen(3000);
+console.log("Corriendo en el puerto 3000");
